4aed40a96af9e6b2281d4eac7cebbab24dd73826
Francisco Nélio <nelio.romero.fn@gmail.com>
2023-10-23 11:22:52 -0300

    sidebar

---
+++ filtro-avancado/src/App.js
@@ -0,0 +1,17 @@
+import Navigation from "./Navigation/Nav"
+import Recommended from "./Recommended/Recommended";
+import Products from "./Products/Products";
+import Sidebar from "./Sidebar/Sidebar";
+
+
+function App() {
+  return <>
+  <Sidebar/>
+  <Navigation/>
+  <Recommended/>
+  <Products/>
+ 
+    </>   
+}
+
+export default App;
